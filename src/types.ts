5de664692802608abe22d3ac7cb3d4a7bab60540
jake currie <jakecurrie1@gmail.com>
2024-11-03 18:54:00 -0800

    add types

---
+++ src/types.ts
@@ -0,0 +1,36 @@
+export interface User {
+    id: string;
+    email: string;
+    name: string;
+    role: string
+    apiCalls: number;
+  }
+  
+  export interface LoginData {
+    email: string;
+    password: string;
+  }
+  
+  export interface RegisterData extends LoginData {
+    name: string;
+  }
+  
+  export interface AuthTokens {
+    accessToken: string;
+    refreshToken: string;
+  }
+  
+  export interface AuthResponse extends AuthTokens {
+    user: User;
+  }
+  
+  export interface AuthContextType {
+    user: User | null;
+    isLoading: boolean;
+    error: string | null;
+    login: (data: LoginData) => Promise<void>;
+    register: (data: RegisterData) => Promise<void>;
+    logout: () => Promise<void>;
+    incrementApiCalls: () => boolean;
+  }
+  
