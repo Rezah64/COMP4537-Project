b07812460d57c5bf24f3c3590dad6193f60c01ea
jake currie <jakecurrie1@gmail.com>
2024-11-24 20:08:13 -0800

    fuck nvm

--- src/auth/axios.ts
+++ src/auth/axios.ts
@@ -1,10 +1,9 @@
 import axios from 'axios';
 
 const apiUrl = import.meta.env.VITE_API_GATEWAY_URL;
-const frontendUrl = import.meta.env.VITE_FRONTEND_URL
 
-if (!apiUrl || !frontendUrl) {
-  throw new Error("Auth url & react app url must be defined in env vars.")
+if (!apiUrl) {
+  throw new Error("API Gateway URL must be defined in env vars.");
 }
 
 export const api = axios.create({
@@ -15,19 +14,11 @@ export const api = axios.create({
   withCredentials: true
 });
 
-api.interceptors.request.use((config) => {
-  config.headers['Origin'] = frontendUrl;
-  return config;
-});
-
-
 api.interceptors.response.use(
   (response) => response,
   async (error) => {
-    if (error.response?.status === 401) {
-      if (!window.location.pathname.includes('/login')) {
-        window.location.href = '/login';
-      }
+    if (error.response?.status === 401 && !window.location.pathname.includes('/login')) {
+      window.location.href = '/login';
     }
     return Promise.reject(error);
   }
